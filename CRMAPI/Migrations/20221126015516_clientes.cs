4087246e6cee8e25d3c6d620aae0fc062b23043f
Guilherme <gui.fogo17@gmail.com>
2022-11-25 23:05:12 -0300

    criando migration cliente com CPF e CNPJ

---
+++ CRMAPI/Migrations/20221126015516_clientes.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRMAPI.Migrations
+{
+    public partial class clientes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CNPJ",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CPF",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CNPJ",
+                table: "Clientes");
+
+            migrationBuilder.DropColumn(
+                name: "CPF",
+                table: "Clientes");
+        }
+    }
+}
